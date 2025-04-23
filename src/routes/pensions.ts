544e3e775716772fe961b5c44836c83642d6b671
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 16:34:24 +0100

    feat: add pots search by name route

--- src/routes/pensions.ts
+++ src/routes/pensions.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import { getAllPensionPotsController } from "../controllers/pension/getAllPensionPotsController";
 import { getAllSearchedPensionsController } from "../controllers/pension/getAllSearchedPensionsController";
 import { getAllPotsController } from "../controllers/pension/getAllPotsController";
+import { getPotByNameController } from "../controllers/pension/getPotByNameController";
 
 const router = Router();
 
 router.get("/pension-pots", getAllPensionPotsController);
 router.get("/searched-pensions", getAllSearchedPensionsController);
 router.get("/all-pots", getAllPotsController);
+router.get("/pots/search", getPotByNameController);
 
 export default router;
