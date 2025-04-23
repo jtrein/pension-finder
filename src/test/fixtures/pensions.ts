068cd4e5069203e855de946a2353a053ddf5cba4
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 13:41:13 +0100

    test: add pensions fixture file

---
+++ src/test/fixtures/pensions.ts
@@ -0,0 +1,32 @@
+export const PENSION_POTS_FIXTURE = [
+  {
+    id: "e181e498-9cab-4570-a188-ed699dc5eefd",
+    potName: "Google",
+    annualInterestRate: 0.02,
+    defaultAnnualInterestRate: 0.02,
+    pensionProvider: {
+      name: "Aviva",
+      value: "AVIVA",
+    },
+    amount: 36700,
+    employer: "Google",
+    lastUpdatedAt: "2024-06-13T13:23:55.614Z",
+    monthlyPayment: 335.53,
+    isWorkplacePension: true,
+  },
+  {
+    id: "5de51030-02f3-48c0-bd54-024120bac5ba",
+    potName: "IBM",
+    annualInterestRate: 0.04,
+    defaultAnnualInterestRate: 0.02,
+    pensionProvider: {
+      name: "Scottish Widows",
+      value: "SCOTTISH_WIDOWS",
+    },
+    amount: 20000,
+    employer: "IBM",
+    lastUpdatedAt: "2024-08-05T14:31:20.506Z",
+    monthlyPayment: 0,
+    isWorkplacePension: false,
+  },
+];
