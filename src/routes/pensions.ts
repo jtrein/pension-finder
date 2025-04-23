9f2b6d818b95a33e5d74dbf33971c316590a0629
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 14:39:57 +0100

    feat: add route for all searched pensions

--- src/routes/pensions.ts
+++ src/routes/pensions.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { getAllPensionPotsController } from "../controllers/pension/getAllPensionPotsController";
+import { getAllSearchedPensionsController } from "../controllers/pension/getAllSearchedPensionsController";
 
 const router = Router();
 
 router.get("/pension-pots", getAllPensionPotsController);
+router.get("/searched-pensions", getAllSearchedPensionsController);
 
 export default router;
