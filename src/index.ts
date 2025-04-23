a0c2c263541387909dffebb6354a243d818b1181
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 13:40:53 +0100

    feat: add pension route to app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
 
+import pensionsRoutes from "./routes/pensions";
+
+const BASE_API_PATH = "/api";
 const PORT = 3000;
 
 const app = express();
+
+app.use(BASE_API_PATH, pensionsRoutes);
 
 // Start the server
 app.listen(PORT, () => {
