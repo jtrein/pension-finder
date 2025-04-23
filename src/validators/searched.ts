b08e7930077c70ffd43c3d4ad350738b2cbfb99d
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 14:42:25 +0100

    feat: add searched reusable validator

---
+++ src/validators/searched.ts
@@ -0,0 +1,25 @@
+import z from "zod";
+
+export const SearchedPensionsSchema = z.array(
+  z.object({
+    id: z.string().uuid(),
+    potName: z.string(),
+    policyNumber: z.string().nullable(),
+    annualFee: z.number().nullable(),
+    status: z.enum(["TO_HUNT", "FOUND"]),
+    previousName: z.string().nullable(),
+    previousAddress: z.string(),
+    annualInterestRate: z.number(),
+    defaultAnnualInterestRate: z.number(),
+    pensionProvider: z.object({
+      name: z.string().nullable(),
+      value: z.string().nullable(),
+    }),
+    amount: z.number(),
+    employer: z.string(),
+    foundOn: z.string().datetime(),
+    lastUpdatedAt: z.string().datetime(),
+    monthlyPayment: z.number(),
+    isDraft: z.boolean(),
+  })
+);
