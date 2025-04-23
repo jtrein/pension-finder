85fdd4617f368f8454a5440061b04271ad12ebe6
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 16:33:16 +0100

    test: add all pots fixture

---
+++ src/test/fixtures/allPots.ts
@@ -0,0 +1,8 @@
+import type { PensionData } from "../../types/pensions";
+import { PENSION_POTS_FIXTURE } from "./pensions";
+import { SEARCHED_PENSIONS_FIXTURE } from "./searched";
+
+export const ALL_POTS_FIXTURE: PensionData = {
+  pensionPots: PENSION_POTS_FIXTURE,
+  searchedPensions: SEARCHED_PENSIONS_FIXTURE,
+};
