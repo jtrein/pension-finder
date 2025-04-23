a706944b5067026e31b7e7fc198ed48699ca2ffd
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 14:54:43 +0100

    feat: add route for all pots

--- src/routes/pensions.ts
+++ src/routes/pensions.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { getAllPensionPotsController } from "../controllers/pension/getAllPensionPotsController";
 import { getAllSearchedPensionsController } from "../controllers/pension/getAllSearchedPensionsController";
+import { getAllPotsController } from "../controllers/pension/getAllPotsController";
 
 const router = Router();
 
 router.get("/pension-pots", getAllPensionPotsController);
 router.get("/searched-pensions", getAllSearchedPensionsController);
+router.get("/all-pots", getAllPotsController);
 
 export default router;
