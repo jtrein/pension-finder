9aa638b103802275a47bab6a16c40fce84d7fbc4
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 16:33:57 +0100

    feat: add more granular validators for items

--- src/validators/searched.ts
+++ src/validators/searched.ts
@@ -1,25 +1,27 @@
 import z from "zod";
 
+export const SearchedPensionResultSchema = z.object({
+  id: z.string().uuid(),
+  potName: z.string(),
+  policyNumber: z.string().nullable(),
+  annualFee: z.number().nullable(),
+  status: z.enum(["TO_HUNT", "FOUND"]),
+  previousName: z.string().nullable(),
+  previousAddress: z.string(),
+  annualInterestRate: z.number(),
+  defaultAnnualInterestRate: z.number(),
+  pensionProvider: z.object({
+    name: z.string().nullable(),
+    value: z.string().nullable(),
+  }),
+  amount: z.number(),
+  employer: z.string(),
+  foundOn: z.string().datetime(),
+  lastUpdatedAt: z.string().datetime(),
+  monthlyPayment: z.number(),
+  isDraft: z.boolean(),
+});
+
 export const SearchedPensionsResultSchema = z.array(
-  z.object({
-    id: z.string().uuid(),
-    potName: z.string(),
-    policyNumber: z.string().nullable(),
-    annualFee: z.number().nullable(),
-    status: z.enum(["TO_HUNT", "FOUND"]),
-    previousName: z.string().nullable(),
-    previousAddress: z.string(),
-    annualInterestRate: z.number(),
-    defaultAnnualInterestRate: z.number(),
-    pensionProvider: z.object({
-      name: z.string().nullable(),
-      value: z.string().nullable(),
-    }),
-    amount: z.number(),
-    employer: z.string(),
-    foundOn: z.string().datetime(),
-    lastUpdatedAt: z.string().datetime(),
-    monthlyPayment: z.number(),
-    isDraft: z.boolean(),
-  })
+  SearchedPensionResultSchema
 );
