c49bcfde6a38b28865e921d915429a7c23178507
Jeremiah Naylor-Trein <jeremiahtrein@gmail.com>
2025-04-23 14:09:51 +0100

    feat: add app error handler

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
 
 import pensionsRoutes from "./routes/pensions";
+import { AppError } from "./errors";
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  if (err instanceof AppError) {
+    res.status(err.details.status).json(err.details);
+
+    return;
+  }
+
+  res.status(500).json({ status: 500, message: "Internal server error" });
+};
 
 const BASE_API_PATH = "/api";
 const PORT = 3000;
@@ -9,7 +20,8 @@ const app = express();
 
 app.use(BASE_API_PATH, pensionsRoutes);
 
-// Start the server
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
